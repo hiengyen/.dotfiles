2970c9907c7f77af34ecf77d50b8cf3b95669cb4
hiengyen <ngyentrnghie777@gmail.com>
2025-05-26 22:09:59 +0700

    update nvim

--- nvimConfig/.config/nvim/lua/plugins/colorscheme.lua
+++ nvimConfig/.config/nvim/lua/plugins/colorscheme.lua
@@ -1,21 +1,61 @@
 return {
 	{
-		"folke/tokyonight.nvim",
+		"Mofiqul/dracula.nvim",
+
 		lazy = false,
-		-- priority = 1000,
+		priority = 1000,
+		name = "dracula",
 		opts = {
-			style = "storm",
 			transparent = true,
+			background = "light",
 			styles = {
 				sidebars = "transparent",
 				floats = "transparent",
 			},
+			integrations = {
+				aerial = true,
+				alpha = true,
+				cmp = true,
+				dashboard = true,
+				flash = true,
+				fzf = true,
+				grug_far = true,
+				gitsigns = true,
+				headlines = true,
+				illuminate = true,
+				indent_blankline = { enabled = true },
+				leap = true,
+				lsp_trouble = true,
+				mason = true,
+				markdown = true,
+				mini = true,
+				native_lsp = {
+					enabled = true,
+					underlines = {
+						errors = { "undercurl" },
+						hints = { "undercurl" },
+						warnings = { "undercurl" },
+						information = { "undercurl" },
+					},
+				},
+				navic = {
+					enabled = true,
+					custom_bg = "lualine",
+				},
+				neotest = true,
+				neotree = true,
+				noice = true,
+				notify = true,
+				semantic_tokens = true,
+				snacks = true,
+				telescope = true,
+				treesitter = true,
+				treesitter_context = true,
+				which_key = true,
+			},
 		},
 		config = function(_, opts)
-			vim.cmd("colorscheme tokyonight-moon")
+			vim.cmd("colorscheme dracula")
 		end,
 	},
-	-- { "nvim-tree/nvim-web-devicons", lazy = true },
-	-- { "nvim-lualine/lualine.nvim", lazy = true },
-	-- { "akinsho/bufferline.nvim", lazy = true },
 }
