23837fe942d8f5cbdc5f87d23ace936052e408d7
hiengyen <ngyentrnghie777@gmail.com>
2026-04-10 12:27:24 +0700

    using daracula themes for nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -663,7 +663,7 @@ require('lazy').setup({
         clangd = {},
         gopls = {},
         pyright = {},
-        ruff = {}, -- Bổ sung Ruff cho Python (Linting & Formatting cực nhanh)
+        ruff = {}, -- Add Ruff for Python (lightning fast Linting & Formatting)
         rust_analyzer = {},
         bashls = {},
         nil_ls = {},
@@ -711,8 +711,8 @@ require('lazy').setup({
         'shfmt',  -- Used to format Bash scripts
       })
       
-      -- Bỏ qua cài đặt nil_ls qua Mason (tránh lỗi FHS/biên dịch), 
-      -- chỉ dùng bản của hệ thống gốc (Nixpkgs)
+      -- Skip installing nil_ls via Mason (to avoid FHS/build errors on Nix),
+      -- and rely on the system-provided package (Nixpkgs) instead.
       ensure_installed = vim.tbl_filter(function(name)
         return name ~= "nil_ls"
       end, ensure_installed)
@@ -768,7 +768,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        python = { 'ruff_fix', 'ruff_format' }, -- Format siêu cấp cho Python
+        python = { 'ruff_fix', 'ruff_format' }, -- Supercharged formatting for Python
         c = { 'clang-format' },
         cpp = { 'clang-format' },
         go = { 'goimports', 'gofmt' },
@@ -884,20 +884,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
+    'Mofiqul/dracula.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
+      require('dracula').setup({
+        -- Add dracula customizations if needed
+        italic_comment = false,
+      })
 
       -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'dracula'
     end,
   },
 
